bfc1c8c78458a5d870fbf678ba9bb4c2acac07f9
easyvendingacademy <easyvending.academy@gmail.com>
2026-06-23 16:55:53 +0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,22 @@
-console.log("EasyVending Start Cabinet");
+function openModel(modelName) {
+    document.getElementById("modelTitle").textContent = modelName;
+    document.getElementById("modelCabinet").classList.remove("hidden");
+    document.getElementById("modelCabinet").scrollIntoView({ behavior: "smooth" });
+
+    showTab("start");
+}
+
+function closeModel() {
+    document.getElementById("modelCabinet").classList.add("hidden");
+    window.scrollTo({ top: 0, behavior: "smooth" });
+}
+
+function showTab(tabId) {
+    const tabs = document.querySelectorAll(".tab-content");
+
+    tabs.forEach(function(tab) {
+        tab.classList.remove("active");
+    });
+
+    document.getElementById(tabId).classList.add("active");
+}
